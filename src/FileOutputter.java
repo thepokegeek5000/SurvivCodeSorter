be9f39ab5c7458fa1647abce0d1c8b0fe7cc45f3
thepokegeek5000 <49299969+thepokegeek5000@users.noreply.github.com>
2020-06-04 16:06:16 -0600

    Update FileOutputter.java

--- src/FileOutputter.java
+++ src/FileOutputter.java
@@ -1,6 +1,13 @@
 import java.io.File;
 import java.io.IOException;
+/*
+How to use:
 
+Take surviv.io image function, go to https://beautifier.io/ , paste the JS, and use Ctrl+Enter to beautify the code
+Copy the beautified code to The file "ImageCode" (double click to open)
+Run the program by clicking one of the two play buttons on this file
+After it's finished, open the file "images.txt" and there you go
+ */
 public class FileOutputter {
     public static void main(String[] args) throws IOException {
         CodeReader reader = new CodeReader(new File("ImageCode"));
