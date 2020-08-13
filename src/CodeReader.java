de894ac4c6892383ebe47a91bfe998ae4eae126b
thepokegeek5000 <49299969+thepokegeek5000@users.noreply.github.com>
2020-08-12 21:52:29 -0600

    Revision 4
    * Updated to use 1 less indent as that's now how what the code has
    * Updated data structure and output to have the image URLs

--- src/CodeReader.java
+++ src/CodeReader.java
@@ -11,15 +11,13 @@ import java.util.Scanner;
  */
 public class CodeReader {
     private File codeFile;
-    private String[][] fileArray;
-    /*
+    private String[][][] fileArray;
+/*
     [
-        [asset name, image1, image2, etc],
-        [asset name, image1, image2, etc],
-        etc
+        [[asset name], [url1, url2, etc.], [image1, image2, etc.]],
+        [[asset name], [url1, url2, etc.], [image1, image2, etc.]],
     ]
-    asset name is kept at [0] as an identifyer while all the various images are sorted
-    */
+*/
     private boolean[] canFetch;//[is sorted, file generated]
     private File outFile;
 
@@ -45,41 +43,9 @@ public class CodeReader {
      * Sorts the images and generates file
      */
     public void runAll() throws IOException {
-        sortImages();
-        genFile();
-    }
-
-    /**
-     * Sorts the image code
-     * @throws IOException
-     */
-    public void sortImages() throws IOException {
-        fileArray=genLists(codeFile);
-        sortLists(fileArray);
-        canFetch[0]=true;
-    }
-
-    /**
-     * Generates output as a single file if you have run the sorting function before
-     */
-    public void genFile() throws IOException {
-        if (canFetch[0]) {
-            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("images.txt"), StandardCharsets.UTF_8);
-            for (int i = 0; i<fileArray.length; i++) {
-                for (int j = 0; j<fileArray[i].length; j++) {
-                    if (j==0) {
-                        writer.write("== "+fileArray[i][j]+" ==");
-                    } else {
-                        writer.write("\n* "+fileArray[i][j]);
-                    }
-                }
-                String bonusLine = i==fileArray.length-1?"":"\n";
-                writer.write("\n"+bonusLine);
-            }
-            writer.close();
-            outFile=new File("images.txt");
-            canFetch[1]=true;
-        } else System.out.println("Error: Failed to use sortImages() first");
+        fileArray=genLists(codeFile);           //Generates Data Array
+        sortLists(fileArray); canFetch[0]=true; //Sorts Data Array
+        genFile();                              //Generates File
     }
 
     /**
@@ -89,12 +55,12 @@ public class CodeReader {
      * @throws IOException
      */
     @NotNull
-    private String[][] genLists(File file) throws IOException {
+    private String[][][] genLists(File file) throws IOException {
         File temp = file;
         Scanner in = new Scanner(temp);
-        ArrayList<ArrayList<String>> output = new ArrayList<>(0);
+        ArrayList<ArrayList<ArrayList<String>>> output = new ArrayList<>(0);
 
-        int[] outSlots = {-1, -1};
+        int[] outSlots = {-1, -1, -1};//[asset slot, image slot, file slot]
         String asset = "";
         boolean isFrames = false;
 
@@ -116,26 +82,32 @@ public class CodeReader {
             //Analyse line based on the tab count on the line and other data
             switch (tabs) {
                 case 0:
-                case 1:
                     isFrames=false;
                     break;
-                case 2:
-                    if (!(line.startsWith("        }"))) {
-                        asset=line.substring(8, l-4);
+                case 1:
+                    if (!(line.startsWith("    }"))) {
+                        asset=line.substring(5, l-5);
                         outSlots[0]++;
                         outSlots[1]=0;
-                        output.add(outSlots[0], new ArrayList<>(0));
-                        output.get(outSlots[0]).add(outSlots[1], asset);
+                        outSlots[2]=0;
+                        output.add(outSlots[0], new ArrayList<>());
+                        output.get(outSlots[0]).add(new ArrayList<>());//0 - asset name
+                        output.get(outSlots[0]).add(new ArrayList<>());//1 - files
+                        output.get(outSlots[0]).add(new ArrayList<>());//2 - image names
+                        output.get(outSlots[0]).get(0).add(outSlots[1], asset);
                     }
                     isFrames=false;//Always has a meta section after not frames
                     break;
-                case 3:
-                    isFrames = line.endsWith("frames: {")?true:false;//check if it's frames
+                case 2:
+                    isFrames = line.endsWith("\"frames\": {")?true:false;//check if it's frames
                     break;
-                case 4:
+                case 3:
                     if (isFrames && !(line.endsWith("}")) && !(line.endsWith("},"))) {
+                        output.get(outSlots[0]).get(2).add(outSlots[2], line.substring(13, l-4));
+                        outSlots[2]++;
+                    } else if (!isFrames && line.startsWith("            \"image\":")) {
+                        output.get(outSlots[0]).get(1).add(outSlots[1], line.substring(22, l-2));
                         outSlots[1]++;
-                        output.get(outSlots[0]).add(outSlots[1], line.substring(17, l-4));
                     }
                     break;
                 default: break;
@@ -144,10 +116,13 @@ public class CodeReader {
         }
 
         //Convert output from ArrayList<ArrayList<String>> to String[][]
-        String[][] out = new String[output.size()][];
+        String[][][] out = new String[output.size()][][];
         for (int i=0; i<output.size(); i++) {
-            out[i] = new String[output.get(i).size()];
-            out[i] = output.get(i).toArray(out[i]);
+            out[i] = new String[output.get(i).size()][];
+            for (int j=0; j<output.get(i).size(); j++) {
+                out[i][j] = new String[output.get(i).get(j).size()];
+                out[i][j] = output.get(i).get(j).toArray(out[i][j]);
+            }
         }
         return out;
     }
@@ -156,23 +131,23 @@ public class CodeReader {
      * Takes an array and sorts the strings in it in alphabetical order, except the first value which is an indicator value
      * @param data array
      */
-    private void sortLists(@NotNull String[][] data) {
+    private void sortLists(@NotNull String[][][] data) {
         for (int a=0; a<data.length; a++) {
             String temp;
-            for (int i=1; i<data[a].length; i++) {
-                for (int j=i+1; j<data[a].length; j++) {
-                    if (data[a][i].compareTo(data[a][j]) > 0) {
-                        temp = data[a][i];
-                        data[a][i] = data[a][j];
-                        data[a][j] = temp;
+            for (int i=0; i<data[a][2].length; i++) {
+                for (int j=i+1; j<data[a][2].length; j++) {
+                    if (data[a][2][i].compareTo(data[a][2][j]) > 0) {
+                        temp = data[a][2][i];
+                        data[a][2][i] = data[a][2][j];
+                        data[a][2][j] = temp;
                     }
                 }
             }
         }
         for (int i=0; i<data.length; i++) {
-            String[] temp;
+            String[][] temp;
             for (int j=i+1; j<data.length; j++) {
-                if (data[i][0].compareTo(data[j][0]) > 0) {
+                if (data[i][0][0].compareTo(data[j][0][0]) > 0) {
                     temp = data[i];
                     data[i] = data[j];
                     data[j] = temp;
@@ -180,5 +155,29 @@ public class CodeReader {
             }
         }
         //System.out.println();
+    }
+
+    /**
+     * Generates output as a single file if you have run the sorting function before
+     */
+    public void genFile() throws IOException {
+        if (canFetch[0]) {
+            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("images.txt"), StandardCharsets.UTF_8);
+            for (int i=0; i<fileArray.length; i++) {
+                writer.write("== "+fileArray[i][0][0]+" ==");
+                for (int j=0; j<fileArray[i][1].length; j++) {
+                    writer.write("\n# https://surviv.io/assets/"+fileArray[i][1][j]);
+                }
+                writer.write("\n");
+                for (int k=0; k<fileArray[i][2].length; k++) {
+                    writer.write("\n* "+fileArray[i][2][k]);
+                }
+                String bonusLine = i==fileArray.length-1?"":"\n\n";
+                writer.write("\n"+bonusLine);
+            }
+            writer.close();
+            outFile=new File("imagesBeta.txt");
+            canFetch[1]=true;
+        } else System.out.println("Error: Failed to use sortImages() first");
     }
 }
